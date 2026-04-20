f10ac3977e918f2a882a790725b3caf6f64afedc
ayush180320 <84795921+ayush180320@users.noreply.github.com>
2026-04-20 09:09:23 +0530

    Update cropDetect.js

--- backend/cropDetect.js
+++ backend/cropDetect.js
@@ -3,43 +3,41 @@ const ffmpegPath = require('ffmpeg-static');
 ffmpeg.setFfmpegPath(ffmpegPath);
 
 async function detectCrop(filePath) {
-  const samples = [5, 15, 30, 60];
+  const times = [5,15,30,60];
   let results = [];
 
-  for (let t of samples) {
-    const crop = await runSample(filePath, t);
-    if (crop) results.push(crop);
+  for (let t of times) {
+    const c = await sample(filePath, t);
+    if (c) results.push(c);
   }
 
   return stabilize(results);
 }
 
-function runSample(file, time) {
-  return new Promise((resolve) => {
-    let found = null;
+function sample(file, t) {
+  return new Promise(res=>{
+    let found=null;
 
     ffmpeg(file)
-      .seekInput(time)
-      .frames(50)
-      .outputOptions('-vf', 'cropdetect=limit=24:round=2:reset=0')
-      .output('-f', 'null')
-      .on('stderr', (line) => {
-        const m = line.match(/crop=\d+:\d+:\d+:\d+/);
-        if (m) found = m[0];
+      .seekInput(t)
+      .frames(40)
+      .outputOptions('-vf','cropdetect=limit=24:round=2:reset=0')
+      .output('-f','null')
+      .on('stderr', l=>{
+        const m=l.match(/crop=\d+:\d+:\d+:\d+/);
+        if(m) found=m[0];
       })
-      .on('end', () => resolve(found))
+      .on('end', ()=>res(found))
       .run();
   });
 }
 
-function stabilize(list) {
-  const freq = {};
-  list.forEach(c => freq[c] = (freq[c] || 0) + 1);
-
-  const best = Object.entries(freq).sort((a,b)=>b[1]-a[1])[0][0];
-  const [w,h,x,y] = best.replace('crop=','').split(':').map(Number);
-
-  return { width:w, height:h, x, y };
+function stabilize(list){
+  const freq={};
+  list.forEach(c=>freq[c]=(freq[c]||0)+1);
+  const best=Object.entries(freq).sort((a,b)=>b[1]-a[1])[0][0];
+  const [w,h,x,y]=best.replace('crop=','').split(':').map(Number);
+  return {width:w,height:h,x,y};
 }
 
-module.exports = { detectCrop };
+module.exports={detectCrop};
