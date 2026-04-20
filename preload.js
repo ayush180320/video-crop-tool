5a0c0b629578cf5f9f009c775e3acca6e0d5af24
ayush180320 <84795921+ayush180320@users.noreply.github.com>
2026-04-20 08:35:30 +0530

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,3 +1,13 @@
-getPresets: () => ipcRenderer.invoke('get-presets'),
-savePreset: (name, crop) => ipcRenderer.invoke('save-preset', name, crop),
-deletePreset: (name) => ipcRenderer.invoke('delete-preset', name)
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('api', {
+  openFile: () => ipcRenderer.invoke('open-file'),
+  playVideo: (file) => ipcRenderer.invoke('play-video', file),
+  detectCrop: (file) => ipcRenderer.invoke('detect-crop', file),
+
+  getPresets: () => ipcRenderer.invoke('get-presets'),
+  savePreset: (name, crop) => ipcRenderer.invoke('save-preset', name, crop),
+  deletePreset: (name) => ipcRenderer.invoke('delete-preset', name),
+
+  generateQC: (meta, crop) => ipcRenderer.invoke('generate-qc', meta, crop)
+});
