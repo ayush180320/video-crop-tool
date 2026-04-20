4c45a8ca587b55d2f906e5915ff3ecc122dc4492
ayush180320 <84795921+ayush180320@users.noreply.github.com>
2026-04-20 07:21:25 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ function createWindow() {
     height: 800,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
-    },
+      contextIsolation: true
+    }
   });
 
   win.loadFile('public/index.html');
