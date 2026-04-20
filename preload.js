64f06a3d668848adac2e7696f5986223ad4364eb
ayush180320 <84795921+ayush180320@users.noreply.github.com>
2026-04-20 09:30:22 +0530

    Update preload.js

--- preload.js
+++ preload.js
@@ -3,11 +3,16 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('api', {
   openFile: () => ipcRenderer.invoke('open-file'),
   playVideo: (f) => ipcRenderer.invoke('play-video', f),
+
+  pause: () => ipcRenderer.invoke('mpv-pause'),
+  frameStep: () => ipcRenderer.invoke('mpv-frame'),
+  seek: (s) => ipcRenderer.invoke('mpv-seek', s),
+
   detectCrop: (f) => ipcRenderer.invoke('detect-crop', f),
 
   getPresets: () => ipcRenderer.invoke('get-presets'),
-  savePreset: (n,c) => ipcRenderer.invoke('save-preset', n, c),
+  savePreset: (n,c) => ipcRenderer.invoke('save-preset', n,c),
   deletePreset: (n) => ipcRenderer.invoke('delete-preset', n),
 
-  generateQC: (m,c) => ipcRenderer.invoke('generate-qc', m, c)
+  generateQC: (m,c) => ipcRenderer.invoke('generate-qc', m,c)
 });
