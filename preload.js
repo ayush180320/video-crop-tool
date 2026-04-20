41cd53b3a82a7b2d2b2b7c52ba382461c2ed8477
ayush180320 <84795921+ayush180320@users.noreply.github.com>
2026-04-20 07:42:22 +0530

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,5 +1,6 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('api', {
+  openFile: () => ipcRenderer.invoke('open-file'),
   detectCrop: (filePath) => ipcRenderer.invoke('detect-crop', filePath)
 });
