9e7485aa4590acc0ce1e734d7e6773b23dc0f362
ayush180320 <84795921+ayush180320@users.noreply.github.com>
2026-04-20 09:09:08 +0530

    Update mpvController.js

--- backend/mpvController.js
+++ backend/mpvController.js
@@ -3,10 +3,7 @@ const path = require('path');
 
 function startMPV(filePath) {
   const mpvPath = path.join(process.resourcesPath, 'mpv', 'mpv.exe');
-
-  spawn(mpvPath, [filePath], {
-    detached: true
-  });
+  spawn(mpvPath, [filePath], { detached: true });
 }
 
 module.exports = { startMPV };
