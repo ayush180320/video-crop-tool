bb12f6f4ad3438665c80c425874fe89542374f88
ayush180320 <84795921+ayush180320@users.noreply.github.com>
2026-04-20 09:30:38 +0530

    Update mpvController.js

--- backend/mpvController.js
+++ backend/mpvController.js
@@ -1,9 +1,37 @@
 const { spawn } = require('child_process');
 const path = require('path');
+const net = require('net');
+
+let mpvProcess = null;
+let socket = null;
+
+const PIPE = '\\\\.\\pipe\\mpv-pipe';
 
 function startMPV(filePath) {
   const mpvPath = path.join(process.resourcesPath, 'mpv', 'mpv.exe');
-  spawn(mpvPath, [filePath], { detached: true });
+
+  if (mpvProcess) mpvProcess.kill();
+
+  mpvProcess = spawn(mpvPath, [
+    filePath,
+    '--force-window=yes',
+    '--border=no',
+    '--ontop=yes',
+    `--input-ipc-server=${PIPE}`
+  ]);
+
+  setTimeout(() => {
+    socket = net.createConnection(PIPE);
+  }, 1000);
 }
 
-module.exports = { startMPV };
+function send(cmd) {
+  if (!socket) return;
+  socket.write(JSON.stringify({ command: cmd }) + '\n');
+}
+
+function pause() { send(['cycle','pause']); }
+function frameStep() { send(['frame-step']); }
+function seek(s) { send(['seek', s, 'relative']); }
+
+module.exports = { startMPV, pause, frameStep, seek };
